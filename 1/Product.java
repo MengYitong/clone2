f92726c7cfa972cc53c1ec982dd4c91210439f5e
MengYitong <mengyitongge@163.com>
2014-04-20 16:02:36 +0800

    fg
    dth

---
+++ 1/Product.java
@@ -0,0 +1,36 @@
+package myHibernate.bean;
+//hehehehehehehehehhehehehehhhe
+public class Product {
+	private String id=null;
+	private String pname=null;
+	private String provider=null;
+	public void setId(String id)
+	{
+		this.id=id;
+	}
+	public String getid()
+	{
+		return this.id;
+		
+	}
+	public void setPname(String pname)
+	{
+		this.pname=pname;
+	}
+	public String getPname()
+	{
+		return this.pname;
+		
+	}
+	public void setProvider(String provider)
+	{
+		this.provider=provider;
+	}
+	public String getProvider()
+	{
+		return this.provider;
+		
+	}
+	
+
+}
